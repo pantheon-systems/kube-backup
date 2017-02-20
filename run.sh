ae13e5c237d697260e4ca7df4626b413366d69d5
joe miller <joeym@joeym.net>
2017-02-19 16:40:41 -0800

    initial commit

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Loop forever, running ./backup.sh every $INTERVAL
+
+set -eou pipefail
+
+INTERVAL="${INTERVAL:-1h}"
+
+gcloud auth activate-service-account "--key-file=$GOOGLE_APPLICATION_CREDENTIALS"
+
+while true; do
+    echo "Executing backup.sh @ $(date)"
+    bash ./backup.sh
+
+    echo "Sleeping for $INTERVAL"
+    sleep "$INTERVAL"
+done
