3ff3112b99250c5ee1143f4bc7f560ba3227ca21
joe miller <joeym@joeym.net>
2017-02-22 14:12:17 -0800

    use absolute path to /app. use explicit google service-account if env var is set

--- run.sh
+++ run.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
-#
-# Loop forever, running ./backup.sh every $INTERVAL
+# Loop forever, running /app/backup.sh every $INTERVAL
 
-set -eou pipefail
+set -eo pipefail
 
 INTERVAL="${INTERVAL:-4h}"
 
-gcloud auth activate-service-account "--key-file=$GOOGLE_APPLICATION_CREDENTIALS"
+# NOTE this only works with the JSON file download, not p12
+if [[ -n "$GOOGLE_APPLICATION_CREDENTIALS" ]]; then
+    gcloud auth activate-service-account "--key-file=$GOOGLE_APPLICATION_CREDENTIALS"
+fi
 
 while true; do
     echo "Executing backup.sh @ $(date)"
-    bash ./backup.sh
+
+    bash /app/backup.sh
 
     echo "Sleeping for $INTERVAL"
     sleep "$INTERVAL"
